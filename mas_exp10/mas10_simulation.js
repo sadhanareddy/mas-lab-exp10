7e1857fbd715bc0b33ffc1376c3cf8f314621b1a
sadhanareddy <sadhana.virupaksha@gmail.com>
2016-07-29 17:18:38 +0530

    modified file

--- mas_exp10/mas10_simulation.js
+++ mas_exp10/mas10_simulation.js
@@ -10,6 +10,15 @@ var frameimages = ["simulation_images/frames/2.png", "simulation_images/frames/3
 "simulation_images/frames/38.png" ];
 		
 
+var framecurves = ["simulation_images/frames/200.png", "simulation_images/frames/201.png", "simulation_images/frames/202.png", 
+"simulation_images/frames/203.png",
+"simulation_images/frames/204.png", "simulation_images/frames/205.png", "simulation_images/frames/206.png", "simulation_images/frames/207.png", 
+"simulation_images/frames/208.png", "simulation_images/frames/209.png" , "simulation_images/frames/210.png", "simulation_images/frames/211.png", 
+"simulation_images/frames/212.png", "simulation_images/frames/213.png", "simulation_images/frames/214.png", "simulation_images/frames/215.png",
+"simulation_images/frames/216.png", "simulation_images/frames/217.png", "simulation_images/frames/218.png", "simulation_images/frames/219.png", 
+"simulation_images/frames/220.png", "simulation_images/frames/221.png", "simulation_images/frames/222.png"]
+
+
 function changeImage(){
 	for(imageindex=0; imageindex < frameimages.length; imageindex++ )
 	{
@@ -17,12 +26,13 @@ function changeImage(){
 		    document.getElementById("frame1").src = frameimages[imageindex];
 		
 	}
-	setTimeout(function(){slideCube()}, 1000);
-	setTimeout(function(){showClock()}, 2000);
+	setTimeout(slideCube(), 1000);
+	setTimeout(showClock(), 2000);
 	setTimeout(function() {
 		document.getElementById("instruction").style.visibility = "visible";
-		$("#instruction").animate({bottom: '60px'})
-	},3000); 
+		$("#instruction").animate({bottom: '60px'}, 500);
+	},7000); 
+	// setTimeout(changeCur(), 8000)
 }
 
 function slideCube() {
@@ -55,5 +65,33 @@ function showClock(){
  		document.getElementById('clockscreen').style.visibility='hidden';
  		document.getElementById('clockhand').style.visibility='hidden';
  	},6245);
+ 	
 }
 
+function changeCur(){
+	document.getElementById("frame1").style.visibility = "hidden";
+	document.getElementById("cube").style.visibility = "hidden";
+	document.getElementById("clockhand").style.visibility = "hidden";
+	document.getElementById("clockscreen").style.visibility = "hidden";
+	document.getElementById("instruction").style.visibility = "hidden";
+
+	// for(i = 0; i < framecurves.length; i++){
+
+	// }
+}
+
+
+// var myIndex = 0;
+
+
+// // function carousel() {
+// //     var i;
+// //     var x = document.getElementsByClassName("mySlides");
+// //     for (i = 0; i < x.length; i++) {
+// //        x[i].style.display = "none";  
+// //     }
+// //     myIndex++;
+// //     if (myIndex > x.length) {myIndex = 1}    
+// //     x[myIndex-1].style.display = "block";  
+// //     setTimeout(carousel, 100); // Change image every 2 seconds
+// // }
